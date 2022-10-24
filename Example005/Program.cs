54ba681a58b0432132aec7af60177548ecab4798
Parshikov Anton <riderinfiniti@yandex.ru>
2022-10-24 21:15:06 +0300

    Add Example005

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,12 +1,16 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
-
-if(username.ToLower == "маша")
-{
-    Console.WriteLine("Ура, это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+﻿// internal class Program
+// {    private static void Main(string[] args)
+//     {
+//         Console.Write("Введите имя пользователя: ");
+//         string username = Console.ReadLine();
+//         if (username.ToLower == "маша")
+//         {
+//             Console.WriteLine("Ура, это же МАША!");
+//         }
+//         else
+//         {
+//             Console.Write("Привет, ");
+//             Console.WriteLine(username);
+//         }
+//     }
+// }
